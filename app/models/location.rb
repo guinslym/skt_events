ac444ea58aa11dd1f2666f910c2f36e8464c7953
Guinsly Mondesir <guinslym@gmail.com>
2014-02-13 22:08:45 -0500

    The map have been display part_1

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :parkname, :suburbs
   has_many :events
+
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode
 end
