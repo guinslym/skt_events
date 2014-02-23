b2a2c7dae0ae21023a5d0a03cf8be7337eda841b
Guinsly Mondesir <guinslym@gmail.com>
2014-02-23 14:42:12 -0500

    remove the subscription in the controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -16,25 +16,4 @@ class FollowsController < ApplicationController
     end
   end
 
-  #subscribing to an event
-  def subscribe
-    current_user.id == params[:user_id]
-    event = Event.fing(params[:event_id])
-    #check to see if the user is following the event
-    #if not create it the subscrition
-    unless current_user.following?(event)
-      user.follow(event)
-    end
-  end
-
-    # UNsubscribing to an event
-  def unsubscribe
-    current_user.id == params[:user_id]
-    event = Event.fing(params[:event_id])
-    #check to see if the user is following the event
-    #if not create it the subscrition
-    if current_user.following?(event)
-      user.stop_following(event)
-    end
-  end
 end
