f143ddb6c28d309e748da55981304c2effb11483
Guinsly Mondesir <guinslym@gmail.com>
2014-03-11 14:41:26 -0400

    remove secret_token from .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SktEvents::Application.config.secret_token = '53a89a891a8ecc8938c659f7f42e5577afb3d33bad4c0fab34716f335ee06318f7c633b14f916560068e5963989286e09ff64f81b94ee225cd4b9b8fc29cc8b5'
+
